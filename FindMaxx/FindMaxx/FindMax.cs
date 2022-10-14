f91b53cb5001379a474f07e8c4ba3cc64560a5f7
nrp033 <pawarnitin9433@gmail.com>
2022-10-14 15:16:16 +0530

    Integer Object and compareTo method to test the maximum number

---
+++ FindMaxx/FindMaxx/FindMax.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaxx
+{
+    internal class FindMax
+    {
+        public int FindMaxInteger(int FirstNum, int SecondNum, int ThirdNum)
+        {
+
+            if (FirstNum.CompareTo(SecondNum) > 0 && FirstNum.CompareTo(ThirdNum) > 0)
+            {
+                return FirstNum;
+            }
+            if (SecondNum.CompareTo(FirstNum) > 0 && SecondNum.CompareTo(ThirdNum) > 0)
+            {
+                return SecondNum;
+            }
+            if (ThirdNum.CompareTo(FirstNum) > 0 && ThirdNum.CompareTo(SecondNum) > 0)
+            {
+                return ThirdNum;
+            }
+            return 0;
+
+        }
+    }
+}
---
+++ FindMaxx/FindMaxx/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace FindMaxx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FindMax findMax = new FindMax();
+
+            int result = findMax.FindMaxInteger(150, 100, 5);
+
+            Console.WriteLine("Greatest number among three numbers is : " + result);
+
+            Console.ReadKey();
+        }
+    }
+}
