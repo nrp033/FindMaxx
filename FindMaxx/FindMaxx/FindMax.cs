5d467afaf9f4b7d863b8a5c0ac3646fb5f518daa
nrp033 <pawarnitin9433@gmail.com>
2022-10-14 16:38:08 +0530

    Refactoring All the 3 Method to One generic Method to Fing Maximum

--- FindMaxx/FindMaxx/FindMax.cs
+++ FindMaxx/FindMaxx/FindMax.cs
@@ -8,58 +8,25 @@ namespace FindMaxx
 {
     internal class FindMax
     {
-        public int FindMaxInteger(int FirstNum, int SecondNum, int ThirdNum)
-        {
+        public T FindMaxAll <T>(T First, T Second, T Third) where T:IComparable
+        { 
 
-            if (FirstNum.CompareTo(SecondNum) > 0 && FirstNum.CompareTo(ThirdNum) > 0)
+            if (First.CompareTo(Second) > 0 && First.CompareTo(Third) > 0)
             {
-                return FirstNum;
+                return First;
             }
-            if (SecondNum.CompareTo(FirstNum) > 0 && SecondNum.CompareTo(ThirdNum) > 0)
+            if (Second.CompareTo(First) > 0 && Second.CompareTo(Third) > 0)
             {
-                return SecondNum;
+                return Second;
             }
-            if (ThirdNum.CompareTo(FirstNum) > 0 && ThirdNum.CompareTo(SecondNum) > 0)
+            if (Third.CompareTo(First) > 0 && Third.CompareTo(Second) > 0)
             {
-                return ThirdNum;
-            }
-            return 0;
-
-        }
-
-        internal float FindMaxFloat(float Value1, float Value2, float Value3)
-        {
-
-            if (Value1.CompareTo(Value2) > 0 && Value1.CompareTo(Value3) > 0)
-            {
-                return Value1;
-            }
-            if (Value2.CompareTo(Value1) > 0 && Value2.CompareTo(Value3) > 0)
-            {
-                return Value2;
-            }
-            if (Value3.CompareTo(Value1) > 0 && Value3.CompareTo(Value2) > 0)
-            {
-                return Value3;
-            }
-            return 0;
-        }
-
-        internal string FindMaxString(string s1, string s2, string s3)
-        {
-            if (s1.CompareTo(s2) > 0 && s1.CompareTo(s3) > 0)
-            {
-                return s1;
-            }
-            if (s2.CompareTo(s1) > 0 && s2.CompareTo(s3) > 0)
-            {
-                return s2;
-            }
-            if (s3.CompareTo(s1) > 0 && s3.CompareTo(s2) > 0)
-            {
-                return s3;
+                return Third;
             }
             return default;
+
         }
+
+       
     }
 }
--- FindMaxx/FindMaxx/Program.cs
+++ FindMaxx/FindMaxx/Program.cs
@@ -27,17 +27,17 @@ namespace FindMaxx
                       
 
                     case 1:
-                        int result = findMax.FindMaxInteger(15, 100, 5);
+                        int result = findMax.FindMaxAll(15, 100, 5);
                         Console.WriteLine("\nGreatest number among three numbers is : " + result);
                         break;
 
                         case 2:
-                        float result1 = findMax.FindMaxFloat(322.3f,433.7f,683.2f);
+                        float result1 = findMax.FindMaxAll(322.3f,433.7f,683.2f);
                         Console.WriteLine("\nGreatest number among three Float Numbers is : " + result1);
                         break;
 
                     case 3:
-                        string result3 = findMax.FindMaxString( "Apple",  "Banana","Peach");
+                        string result3 = findMax.FindMaxAll( "Apple",  "Banana","Peach");
                         Console.WriteLine("\nGreatest string among three is : " + result3);
                         break;
 
